d6f81307e7f68be2d7d4e9cb2880c747f68ffe26
David Hughes <39618866+dh85@users.noreply.github.com>
2021-03-16 15:19:00 +0000

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by David Hughes on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
